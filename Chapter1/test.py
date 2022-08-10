c38560be9ea58d857d942d57f1b7619486c2f07f
Bocanegra Hernandez-EXT <uie82661@contiwan.com>
2022-08-10 12:16:08 -0500

    Comit 2- Chapter1

--- Chapter1/test.py
+++ Chapter1/test.py
@@ -1,5 +1,13 @@
-z384z3lz3xy
-z3b4zz3xy
-z3b4z3lz3xy
+print("hola")
+print("ADIOS")
+#python archivo.py
+# las variables se crean cuando se le asigna un valor
 
-este es un mensaje nuevo
+numero = 5
+cadena = "Try String"
+cadena2 = "Try String2"
+cadenaNueva= cadena + cadena2
+print(numero)
+print(cadenaNueva)
+
+type(cadenaNueva)
